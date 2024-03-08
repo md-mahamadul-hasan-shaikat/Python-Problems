6c843a14c47491fa8b7f7d1838397885336db92d
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2024-03-09 00:20:55 +0600

    Area of Shape

---
+++ area_shape.py
@@ -0,0 +1,15 @@
+'''
+base = float(input("Enter Base : "))
+heignt = float(input("Enter Height : "))
+
+area = 0.5 * base * heignt
+
+print("Area is : ", area)
+
+'''
+
+radius = float(input("Enter Radius : "))
+
+area = 3.1416 * radius * radius
+
+print("Area of Circle is : ", area)
