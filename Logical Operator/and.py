e96df36549b1d3def2a872dc6ad8ad4470e87edb
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2024-03-09 01:39:26 +0600

    Logical Operator

---
+++ Logical Operator/and.py
@@ -0,0 +1,12 @@
+x =20
+y =10
+z = 70
+
+if x>y and x>z:
+    print(x)
+
+elif y>x and y>z:
+    print(y)
+
+else :
+    print(z)         
---
+++ Logical Operator/or.py
@@ -0,0 +1,7 @@
+ch = 'w'
+
+if ch == 'a' or   ch == 'e' or  ch ==  'i' or  ch ==  'o' or  ch ==  'u':
+    print("Vowel")
+
+else:
+    print("Consonent")    
