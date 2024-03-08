0cefbd0fa30fb7fae99e6eda3dcda6812ab850c4
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2024-03-09 01:27:28 +0600

    If,else,elif,inner if

---
+++ Conditions/if_else.py
@@ -0,0 +1,59 @@
+'''
+num1 = 20
+num2 = 10
+
+if num1>num2:
+   print(num1)
+
+else:
+   print(num2)   
+
+  
+
+#even| odd
+num = 5
+
+if num%2 == 0:
+    print("Even")
+
+else:
+    print("Odd")    
+
+    
+
+
+marks = 85
+
+if marks>= 80:
+    print("A+")
+
+elif marks>= 70:
+    print("A")
+
+elif marks>= 60:
+    print("A-")
+
+elif marks>= 50:
+    print("B")
+elif marks>= 40:
+    print("C")
+elif marks>= 33:
+    print("D")
+else:
+    print("Fail")    
+
+     '''
+
+
+#inner if
+
+if 7>9:
+    if 7>8:
+        print("Hi")
+
+else:
+    print("Hello")
+
+
+
+    
