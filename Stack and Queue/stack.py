5c1140be73bb376ca0fb9eebd0e6ebb5d6fdf1a7
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2024-03-10 00:53:42 +0600

    stack

---
+++ Stack and Queue/stack.py
@@ -0,0 +1,16 @@
+book = []
+book.append("Learn c")
+book.append("Learn c++")
+book.append("Learn Java")
+book.append("Learn Python")
+
+print(book)
+
+book.pop()
+print("Now the top book is : ", book[-1])
+
+book.pop()
+print("Now the top book is : ", book[-1])
+
+book.pop()
+print("Now the top book is : ", book[-1])
