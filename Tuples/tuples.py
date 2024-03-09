4e02121408c270614d458cd96b0ad78ba5f3411c
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2024-03-10 00:41:47 +0600

    Tuples

---
+++ Tuples/tuples.py
@@ -0,0 +1,15 @@
+ # tuple object does not support item assignment
+
+students = (
+
+    "MHS 1",
+    "MHS 2",
+    "MHS 3",
+    "MHS 4",
+    "MHS 5",
+
+)
+
+#students[0] = "abcd"
+
+print(students[0])
