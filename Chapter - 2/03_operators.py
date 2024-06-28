55d0f7ae59b257b345cbdaa00c4d510585056071
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2024-06-29 01:08:19 +0600

    Operators

---
+++ Chapter - 2/03_operators.py
@@ -0,0 +1,64 @@
+#Arithmetic Operator
+print('\nArithmetic Operator')
+a = 5
+b = 4
+c = a+b
+print(c)
+
+
+#Assignment Operator
+print('\nAssignment Operator')
+a = 4-3 #assign 4-3 in a
+print(a)
+b = 6
+b += 3 #increment the value of b by 3 and then assign it to b
+print(b)
+
+
+#Comparison Operator
+print('\nComparison Operator')
+d = 5<4
+print(d)
+
+d = 5>4
+print(d)
+
+d = 5>=5
+print(d)
+
+d = 5<=5
+print(d)
+
+d = 5!=5
+print(d)
+
+d = 5==5
+print(d)
+
+
+
+#Logical Operators
+print('\nLogical Operators')
+e = True or False
+
+#Truth table of 'or'
+print('Truth table of or')
+print('True or False is ', True or False)
+print('True or True is ', True or True)
+print('False or True is ', False or True)
+print('False or False is ', False or False)
+
+
+#Truth table of 'and'
+print('\nTruth table of and')
+print('True and False is ', True and False)
+print('True and True is ', True and True)
+print('False and True is ', False and True)
+print('False and False is ', False and False)
+
+
+#Not Operator
+print('\nNot Operators')
+print(not(False))
+print(not(True))
+
