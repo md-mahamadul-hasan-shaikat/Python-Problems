b3fb6ea5616703bfd65f984f4423f47265f9c4ee
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2024-06-28 21:41:04 +0600

    Install an external module and use it to perform an operation of your inter

--- Chapter - 1/problem3.py
+++ Chapter - 1/problem3.py
@@ -1,7 +1,6 @@
 #Install an external module and use it to perform an operation of your inter
 
-
 import pyttsx3
 engine = pyttsx3.init()
-engine.say("I will speak this text")
+engine.say("I will speak this text me")
 engine.runAndWait()
