682c7db3cc0b22f608707adacf2351e9030ddace
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2024-06-29 01:47:21 +0600

    Write a python program to find an avarage of two number entered by the user.

---
+++ Chapter - 2/Practice Que/05_problem.py
@@ -0,0 +1,5 @@
+#Write a python program to find an avarage of two number entered by the user.
+
+a = int(input('Enter a : '))
+b = int(input('Enter b : '))
+print('Avarage of two numbers: ', (a+b)/2)
